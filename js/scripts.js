e80ec488fcec581b8e47a158c2dd3dcc6279eef7
Alexander Jacks <alexd.jacks@gmail.com>
2016-12-22 14:12:02 -0800

    builds all toggle functions for MVP

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,43 @@
 $(document).ready(function(){
-  $("#salmon_tog").click(function(){
-    $("#salmon").toggle();
+  $("#sunny_tog").click(function(){
+    $("#albacore").toggle();
   });
-  $("#catfish_tog").click(function(){
+  $("#rainy_tog").click(function(){
+    $("#albacore").toggle();
     $("#catfish").toggle();
+    $("#eel").toggle();
   });
+
+  $("#spring_tog").click(function(){
+
+  });
+  $("#summer_tog").click(function(){
+
+  });
+  $("#fall_tog").click(function(){
+    $("#albacore").toggle();
+  });
+  $("#winter_tog").click(function(){
+    $("#albacore").toggle();
+  });
+
+  $("#day_tog").click(function(){
+    $("#albacore").toggle();
+  });
+  $("#night_tog").click(function(){
+    $("#albacore").toggle();
+  });
+
+
 
 });
 
-// $("#salmon").toggle();
-// $("#catfish").toggle();
-// $("#redmullet").toggle();
-// $("#eel").toggle();
-// $("#sturgeon").toggle();
-// $("#tilapia").toggle();
-// $("#albacore").toggle();
-// $("#shad").toggle();
-// $("#chub").toggle();
-// $("#crab").toggle();
+$("#catfish").toggle();
+$("#chub").toggle();
+$("#crab").toggle();
+$("#eel").toggle();
+$("#redmullet").toggle();
+$("#salmon").toggle();
+$("#shad").toggle();
+$("#sturgeon").toggle();
+$("#tilapia").toggle();
