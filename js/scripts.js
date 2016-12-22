66868773f7f102ae6d5c85e49544fbf0cd6a706b
Alexander Jacks <alexd.jacks@gmail.com>
2016-12-22 14:39:00 -0800

    adds methods & styling for MVP

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,69 @@
 $(document).ready(function(){
   $("#sunny_tog").click(function(){
     $("#albacore").toggle();
+
   });
   $("#rainy_tog").click(function(){
     $("#albacore").toggle();
     $("#catfish").toggle();
     $("#eel").toggle();
+    $("#shad").toggle();
+
   });
 
   $("#spring_tog").click(function(){
+    $("#catfish").toggle();
+    $("#eel").toggle();
+    $("#shad").toggle();
 
   });
   $("#summer_tog").click(function(){
+    $("#chub").toggle();
+    $("#octopus").toggle();
+    $("#redmullet").toggle();
+    $("#shad").toggle();
+    $("#sturgeon").toggle();
+    $("#tilapia").toggle();
 
   });
   $("#fall_tog").click(function(){
     $("#albacore").toggle();
+    $("#catfish").toggle();
+    $("#eel").toggle();
+    $("#salmon").toggle();
+    $("#shad").toggle();
+    $("#tilapia").toggle();
+
   });
   $("#winter_tog").click(function(){
     $("#albacore").toggle();
+    $("#redmullet").toggle();
+    $("#sturgeon").toggle();
+
   });
 
   $("#day_tog").click(function(){
     $("#albacore").toggle();
+    $("#tilapia").toggle();
+
   });
   $("#night_tog").click(function(){
     $("#albacore").toggle();
+    $("#eel").toggle();
+
   });
 
 
 
 });
 
-$("#catfish").toggle();
-$("#chub").toggle();
-$("#crab").toggle();
-$("#eel").toggle();
-$("#redmullet").toggle();
-$("#salmon").toggle();
-$("#shad").toggle();
-$("#sturgeon").toggle();
-$("#tilapia").toggle();
+// $("#albacore").toggle();
+// $("#catfish").toggle();
+// $("#chub").toggle();
+// $("#eel").toggle();
+// $("#octopus").toggle();
+// $("#redmullet").toggle();
+// $("#salmon").toggle();
+// $("#shad").toggle();
+// $("#sturgeon").toggle();
+// $("#tilapia").toggle();
